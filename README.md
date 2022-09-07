3c0de53dc1962bbe0f57601523f7fc9a414758f3
dch23 <dcvweb123@gmail.com>
2022-09-06 20:49:51 -0400

    tech to skill

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## Personal Portfolio
 
 ![Portfolio Website](https://i.ibb.co/WgPMpts/image.png)
+
+Deploy to Netlify:
+1. cd
+2. ntl deploy
+3. out
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,7 +23,7 @@ const Header = () =>  (
 			</li>
 			<li>
 				<Link href="#tech">
-					<NavLink>Technologies</NavLink>
+					<NavLink>Skills</NavLink>
 				</Link>
 			</li>
 			<li>
