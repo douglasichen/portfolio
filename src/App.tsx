7f360534da180e56c8d8f35d5ee3f05ab1502779
Douglas <douglas@ichen.ca>
2025-05-10 12:12:47 -0400

    feat: setup

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="layout">
+            <div className="content">
+                <div className="left-pane">
+                    <h1>Douglas Chen</h1>
+                    <h2>Back End Engineer</h2>
+                    <p>I love solving problems</p>
+                </div>
+                <div className="right-pane">
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                    <p>PARAGRAPH HERE</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
