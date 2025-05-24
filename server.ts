fac3db959a3d7aaf4b62ebea0b4ce61ccd6059d4
Douglas Chen <douglas.chen@insidedesk.com>
2025-05-24 17:19:05 -0400

    chore: fmt and add analytics tag

--- server.ts
+++ server.ts
@@ -3,8 +3,8 @@ import { serveDir } from "jsr:@std/http@^1.0.0/file-server";
 
 // @ts-ignore
 Deno.serve((req: Request) => {
-  return serveDir(req, {
-    fsRoot: "dist",
-    urlRoot: "",
-  });
+    return serveDir(req, {
+        fsRoot: "dist",
+        urlRoot: "",
+    });
 });
